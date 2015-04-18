0bd5c28e2066f374812b4afa615b2bc011f146d7
Ryuzaki <ojizaki@gmail.com>
2015-04-19 00:07:55 +0200

    stuff

---
+++ Assets/Scripts/RedDamageBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedDamageBehaviour : MonoBehaviour {
+
+    private Animator _animator;
+
+    void Start() 
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void EndAnimationEvent() {
+        _animator.SetBool("ActivateRedDamageIndicator", false);
+    }
+
+    public void BeginAnimation() {
+        _animator.SetBool("ActivateRedDamageIndicator", true);
+    }
+}
---
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponBehaviour : MonoBehaviour {
+
+    // Make damage to the game object collided with
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+            col.gameObject.AddComponent<EnemyBehaviour>().Hitted();
+    }
+
+    public void BeginAttackEvent(){
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void EndAttackEvent()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
